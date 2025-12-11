f84e4f551425b1a69e8e0d98268319b382dfbc3f
anuraaagg <s.anurag1998@gmail.com>
2025-12-11 12:28:16 +0530

    Initial Commit

---
+++ musicglobe/musicglobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicglobeApp.swift
+//  musicglobe
+//
+//  Created by Anurag Singh on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct musicglobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
